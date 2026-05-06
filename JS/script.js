bc2e5a4830e4c0a852fe72c0d12f2e2598396d65
Jack <heajac29@ecolecatholique.ca>
2026-05-06 13:44:35 -0400

    changes

--- JS/script.js
+++ JS/script.js
@@ -2,7 +2,7 @@ var x = 0
 function test() {
     var affiche = document.getElementById("divAffiche");
     card = "hello";
-    console.log = "hello";
+    console.log = x;
     x = 1;
     affiche.innerHTML = card;
 }
