9594a3a9b27f7f03d74dac68b8545802724b9f9d
Jack <heajac29@ecolecatholique.ca>
2026-05-06 13:58:48 -0400

    changes

--- JS/script.js
+++ JS/script.js
@@ -1,8 +1,8 @@
 
 function flipCard1() {
-    var affiche = document.getElementById("divAffiche");
-    card = "hello";
     //source = https://www.w3schools.com/JSREF/prop_pushbutton_value.asp
-    document.getElementById("card1").value = 2
-    affiche.innerHTML = card;
+    document.getElementById("card1").value = 2 + 2
+}
+function flipCard2() {
+    document.getElementById("card2").value = 4
 }
