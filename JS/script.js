535c69051463a322da1643db11ce5b525ad9019f
Jack <heajac29@ecolecatholique.ca>
2026-05-06 13:54:57 -0400

    changes

--- JS/script.js
+++ JS/script.js
@@ -2,6 +2,7 @@
 function test() {
     var affiche = document.getElementById("divAffiche");
     card = "hello";
+    //source = https://www.w3schools.com/JSREF/prop_pushbutton_value.asp
     document.getElementById("card1").value = 2
     affiche.innerHTML = card;
 }
