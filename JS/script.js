fa8e585bb5b6c27ad5afbb8a05af96919ed9799a
Jack <heajac29@ecolecatholique.ca>
2026-05-07 12:17:13 -0400

    asdasdasd

--- JS/script.js
+++ JS/script.js
@@ -1,4 +1,5 @@
 let cardValue = ["0", "1", "2", "3", "4", "5", "6", "7", "8", "9", "10", "11", "12", "13", "14", "15", "16", "17", "18", "r1", "r2", "r3", "r4", "r5", "r6", "r7", "r8", "r9", "r10", "r11", "r12", "r13", "r14", "r15", "r16", "r17", "r18",]
+let cardValueJeu = new cardValueJeu[36];
 var card1Value = 0
 var card1value = 0
 var card2Value = 0
@@ -37,150 +38,15 @@ var card34Value = 0
 var card35Value = 0
 var card36Value = 0
 function randomizer() {
-    let i = parseInt(Math.floor(Math.random() * cardValue.length) + 1)
-    console.log(i)
-    card1Value = cardValue[i]
-    cardValue.splice(i, 1)
-    i = parseInt(Math.floor(Math.random() * cardValue.length) + 1)
-    card2Value = cardValue[i]
-    console.log(i)
-    cardValue.splice(i, 1)
-    i = parseInt(Math.floor(Math.random() * cardValue.length) + 1)
-    card3Value = cardValue[i]
-    console.log(i)
-    cardValue.splice(i, 1)
-    i = parseInt(Math.floor(Math.random() * cardValue.length) + 1)
-    card4Value = cardValue[i]
-    console.log(i)
-    cardValue.splice(i, 1)
-    i = parseInt(Math.floor(Math.random() * cardValue.length) + 1)
-    card5Value = cardValue[i]
-    console.log(i)
-    cardValue.splice(i, 1)
-    i = parseInt(Math.floor(Math.random() * cardValue.length) + 1)
-    card6Value = cardValue[i]
-    console.log(i)
-    cardValue.splice(i, 1)
-    i = parseInt(Math.floor(Math.random() * cardValue.length) + 1)
-    card7Value = cardValue[i]
-    console.log(i)
-    cardValue.splice(i, 1)
-    i = parseInt(Math.floor(Math.random() * cardValue.length) + 1)
-    card8Value = cardValue[i]
-    console.log(i)
-    cardValue.splice(i, 1)
-    i = parseInt(Math.floor(Math.random() * cardValue.length) + 1)
-    card9Value = cardValue[i]
-    console.log(i)
-    cardValue.splice(i, 1)
-    i = parseInt(Math.floor(Math.random() * cardValue.length) + 1)
-    card10Value = cardValue[i]
-    console.log(i)
-    cardValue.splice(i, 1)
-    i = parseInt(Math.floor(Math.random() * cardValue.length) + 1)
-    card11Value = cardValue[i]
-    console.log(i)
-    cardValue.splice(i, 1)
-    i = parseInt(Math.floor(Math.random() * cardValue.length) + 1)
-    card12Value = cardValue[i]
-    console.log(i)
-    cardValue.splice(i, 1)
-    i = parseInt(Math.floor(Math.random() * cardValue.length) + 1)
-    card13Value = cardValue[i]
-    console.log(i)
-    cardValue.splice(i, 1)
-    i = parseInt(Math.floor(Math.random() * cardValue.length) + 1)
-    card14Value = cardValue[i]
-    console.log(i)
-    cardValue.splice(i, 1)
-    i = parseInt(Math.floor(Math.random() * cardValue.length) + 1)
-    card15Value = cardValue[i]
-    console.log(i)
-    cardValue.splice(i, 1)
-    i = parseInt(Math.floor(Math.random() * cardValue.length) + 1)
-    card16Value = cardValue[i]
-    console.log(i)
-    cardValue.splice(i, 1)
-    i = parseInt(Math.floor(Math.random() * cardValue.length) + 1)
-    card17Value = cardValue[i]
-    console.log(i)
-    cardValue.splice(i, 1)
-    i = parseInt(Math.floor(Math.random() * cardValue.length) + 1)
-    card18Value = cardValue[i]
-    console.log(i)
-    cardValue.splice(i, 1)
-    i = parseInt(Math.floor(Math.random() * cardValue.length) + 1)
-    card19Value = cardValue[i]
-    console.log(i)
-    cardValue.splice(i, 1)
-    i = parseInt(Math.floor(Math.random() * cardValue.length) + 1)
-    card20Value = cardValue[i]
-    console.log(i)
-    cardValue.splice(i, 1)
-    i = parseInt(Math.floor(Math.random() * cardValue.length) + 1)
-    card21Value = cardValue[i]
-    console.log(i)
-    cardValue.splice(i, 1)
-    i = parseInt(Math.floor(Math.random() * cardValue.length) + 1)
-    card22Value = cardValue[i]
-    console.log(i)
-    cardValue.splice(i, 1)
-    i = parseInt(Math.floor(Math.random() * cardValue.length) + 1)
-    card23Value = cardValue[i]
-    console.log(i)
-    cardValue.splice(i, 1)
-    i = parseInt(Math.floor(Math.random() * cardValue.length) + 1)
-    card24Value = cardValue[i]
-    console.log(i)
-    cardValue.splice(i, 1)
-    i = parseInt(Math.floor(Math.random() * cardValue.length) + 1)
-    card25Value = cardValue[i]
-    console.log(i)
-    cardValue.splice(i, 1)
-    i = parseInt(Math.floor(Math.random() * cardValue.length) + 1)
-    card26Value = cardValue[i]
-    console.log(i)
-    cardValue.splice(i, 1)
-    i = parseInt(Math.floor(Math.random() * cardValue.length) + 1)
-    card27Value = cardValue[i]
-    console.log(i)
-    cardValue.splice(i, 1)
-    i = parseInt(Math.floor(Math.random() * cardValue.length) + 1)
-    card28Value = cardValue[i]
-    console.log(i)
-    cardValue.splice(i, 1)
-    i = parseInt(Math.floor(Math.random() * cardValue.length) + 1)
-    card29Value = cardValue[i]
-    console.log(i)
-    cardValue.splice(i, 1)
-    i = parseInt(Math.floor(Math.random() * cardValue.length) + 1)
-    card30Value = cardValue[i]
-    console.log(i)
-    cardValue.splice(i, 1)
-    i = parseInt(Math.floor(Math.random() * cardValue.length) + 1)
-    card31Value = cardValue[i]
-    console.log(i)
-    cardValue.splice(i, 1)
-    i = parseInt(Math.floor(Math.random() * cardValue.length) + 1)
-    card32Value = cardValue[i]
-    console.log(i)
-    cardValue.splice(i, 1)
-    i = parseInt(Math.floor(Math.random() * cardValue.length) + 1)
-    card33Value = cardValue[i]
-    console.log(i)
-    cardValue.splice(i, 1)
-    i = parseInt(Math.floor(Math.random() * cardValue.length) + 1)
-    card34Value = cardValue[i]
-    console.log(i)
-    cardValue.splice(i, 1)
-    i = parseInt(Math.floor(Math.random() * cardValue.length) + 1)
-    card35Value = cardValue[i]
-    console.log(i)
-    cardValue.splice(i, 1)
-    i = parseInt(Math.floor(Math.random() * cardValue.length) + 1)
-    card36Value = cardValue[i]
-    console.log(i)
-    cardValue.splice(i, 1)
+    let j;
+    for (let i=0; i<36; i++) {
+        j = parseInt(Math.floor(Math.random() * cardValue.length) + 1)
+        console.log(j + " " + cardValue[j] )
+        cardValueJeu[i] = cardValue[j]
+        cardValue.splice(j, 1) 
+    }
+    
+    
 }
 function flipCard1() {
     //source = https://www.w3schools.com/JSREF/prop_pushbutton_value.asp
