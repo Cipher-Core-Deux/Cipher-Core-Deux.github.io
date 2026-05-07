e42bfc97cebc169cf8ec2086f7742e3c79a21527
Jack <heajac29@ecolecatholique.ca>
2026-05-07 11:40:45 -0400

    changes

--- JS/script.js
+++ JS/script.js
@@ -1,5 +1,6 @@
 let cardValue = ["0", "1", "2", "3", "4", "5", "6", "7", "8", "9", "10", "11", "12", "13", "14", "15", "16", "17", "18", "r1", "r2", "r3", "r4", "r5", "r6", "r7", "r8", "r9", "r10", "r11", "r12", "r13", "r14", "r15", "r16", "r17", "r18",]
 var card1Value = cardValue[i]
+    var card1value = 0
     var card2Value = 0
     var card3Value = 0
     var card4Value = 0
