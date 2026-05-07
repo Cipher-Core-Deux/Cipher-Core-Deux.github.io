594677bd132386819bf28beaf7fb73e038d85d8b
Jack <heajac29@ecolecatholique.ca>
2026-05-07 12:37:38 -0400

    asdfgh

--- JS/script.js
+++ JS/script.js
@@ -2,6 +2,7 @@ let cardValue = ["1", "2", "3", "4", "5", "6", "7", "8", "9", "10", "11", "12", 
 let cardValueJeu = [""];
 function randomizer() {
     let j;
+    cardValueJeu = [""];
     for (let i=0; i<36; i++) {
         j = parseInt(Math.floor(Math.random() * cardValue.length))
         console.log(j + " " + cardValue[j] )
