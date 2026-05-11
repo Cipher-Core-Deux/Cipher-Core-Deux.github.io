3cae8e12eee7785c1a8afedd975e7cdb27f90eca
Jack <heajac29@ecolecatholique.ca>
2026-05-11 10:13:38 -0400

    sad

--- JS/script.js
+++ JS/script.js
@@ -16,6 +16,13 @@ function randomizer() {
 function flipCard1() {
     //source = https://www.w3schools.com/JSREF/prop_pushbutton_value.asp
     document.getElementById("card1").value = cardValueJeu[0]
+    for (let i = 0; i < cardValue.length; i++) {
+        if (i%2 != 0) {
+            if (cardValueJeu[0] == cardValue[i]) {
+                
+            }
+        }
+    }
 }
 function flipCard2() {
     document.getElementById("card2").value = cardValueJeu[1]
