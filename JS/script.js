24baf5b6b59063812f045319da9f249097a6498c
Jack <heajac29@ecolecatholique.ca>
2026-05-11 09:47:32 -0400

    qzawxsedcrfvt

--- JS/script.js
+++ JS/script.js
@@ -1,4 +1,4 @@
-let cardValue = ["1", "2", "3", "4", "5", "6", "7", "8", "9", "10", "11", "12", "13", "14", "15", "16", "17", "18", "r1", "r2", "r3", "r4", "r5", "r6", "r7", "r8", "r9", "r10", "r11", "r12", "r13", "r14", "r15", "r16", "r17", "r18"]
+let cardValue = ["Résous : 2x+7=19", "Réponse : 6", "Factorise : x²-9", "Réponse : (x-3)(x+3)", "Résous : x²-16=0", "x=±4", "Simplifie : (3x^2)(2x^3)", "Réponse : 6x^5", "Quelle est la pente de la droite passant par (2,3) et (6,11) ?", "Réponse : 2", "Développe : (x+5)(x-2)", "Réponse : x^2+3x-10", "Résous : 3(x-4)=18", "Réponse : 10", "Quel est le sommet de y=(x-2)^2+5 ?", "Réponse : (2,5)", "Simplifie : 12x^2/3x", "Réponse : 4x", "Résous : sqrt(x+1)=5", "Réponse : 24", "Factorise : x^2+7x+12", "Réponse : (x+3)(x+4)", "Quelle est la valeur de sin(30∘) ?", "Réponse : 1/2", "Convertis 0.625 en fraction.", "Réponse : 5/8", "Résous : 5x-12=3", "Réponse : 3", "Développe : (2x-1)^2", "Réponse : 4x^2-4x+1", "Calcule : 3^4", "Réponse : 81", "Simplifie : sqrt(72)", "Réponse : 6sqrt(2)", "Quelle est l'équation d'un cercle centré à l'origine avec un rayon de 5 ?", "Réponse : x^2+y^2=25"]
 let cardValueJeu = [""];
 function randomizer() {
     let j;
@@ -11,7 +11,7 @@ function randomizer() {
         console.log(cardValueJeu.toString() )
         cardValue.splice(j, 1) 
     }
-    cardValue = ["1", "2", "3", "4", "5", "6", "7", "8", "9", "10", "11", "12", "13", "14", "15", "16", "17", "18", "r1", "r2", "r3", "r4", "r5", "r6", "r7", "r8", "r9", "r10", "r11", "r12", "r13", "r14", "r15", "r16", "r17", "r18"]
+    cardValue = ["Résous : 2x+7=19", "Réponse : 6", "Factorise : x²-9", "Réponse : (x-3)(x+3)", "Résous : x²-16=0", "x=±4", "Simplifie : (3x^2)(2x^3)", "Réponse : 6x^5", "Quelle est la pente de la droite passant par (2,3) et (6,11) ?", "Réponse : 2", "Développe : (x+5)(x-2)", "Réponse : x^2+3x-10", "Résous : 3(x-4)=18", "Réponse : 10", "Quel est le sommet de y=(x-2)^2+5 ?", "Réponse : (2,5)", "Simplifie : 12x^2/3x", "Réponse : 4x", "Résous : sqrt(x+1)=5", "Réponse : 24", "Factorise : x^2+7x+12", "Réponse : (x+3)(x+4)", "Quelle est la valeur de sin(30∘) ?", "Réponse : 1/2", "Convertis 0.625 en fraction.", "Réponse : 5/8", "Résous : 5x-12=3", "Réponse : 3", "Développe : (2x-1)^2", "Réponse : 4x^2-4x+1", "Calcule : 3^4", "Réponse : 81", "Simplifie : sqrt(72)", "Réponse : 6sqrt(2)", "Quelle est l'équation d'un cercle centré à l'origine avec un rayon de 5 ?", "Réponse : x^2+y^2=25"]
 }
 function flipCard1() {
     //source = https://www.w3schools.com/JSREF/prop_pushbutton_value.asp
