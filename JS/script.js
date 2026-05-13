aac7fda3e5e4ecca79edd0e22a1e27931fd4125a
Jack <heajac29@ecolecatholique.ca>
2026-05-13 11:31:51 -0400

    aaaaaaaaaaaaaaaaa

--- JS/script.js
+++ JS/script.js
@@ -18,7 +18,7 @@ let card = 0
 let score = 0
 function flipCard1() {
     //source = https://www.w3schools.com/JSREF/prop_pushbutton_value.asp
-    document.getElementById("card1").src = cardValue[0]
+    document.getElementById("card1").src = cardValueJeu[0]
     card = 1
     for (let i = 0; i < cardValue.length; i++) {
         if (i%2 != 0) {
@@ -33,110 +33,110 @@ function flipCard1() {
     }
 }
 function flipCard2() {
-    document.getElementById("card2").src = "../Images/cardFrontBlank.png"
+    document.getElementById("card2").src = cardValueJeu[1]
     card = 2
 }
 function flipCard3() {
-    document.getElementById("card3").src = "../Images/cardFrontBlank.png"
+    document.getElementById("card3").src = cardValueJeu[2]
 }
 function flipCard4() {
-    document.getElementById("card4").src = "../Images/cardFrontBlank.png"
+    document.getElementById("card4").src = cardValueJeu[3]
 }
 function flipCard5() {
-    document.getElementById("card5").src = "../Images/cardFrontBlank.png"
+    document.getElementById("card5").src = cardValueJeu[4]
 }
 function flipCard6() {
-    document.getElementById("card6").src = "../Images/cardFrontBlank.png"
+    document.getElementById("card6").src = cardValueJeu[5]
 }
 function flipCard7() {
-    document.getElementById("card7").src = "../Images/cardFrontBlank.png"
+    document.getElementById("card7").src = cardValueJeu[6]
 }
 function flipCard8() {
-    document.getElementById("card8").src = "../Images/cardFrontBlank.png"
+    document.getElementById("card8").src = cardValueJeu[7]
 }
 function flipCard9() {
-    document.getElementById("card9").src = "../Images/cardFrontBlank.png"
+    document.getElementById("card9").src = cardValueJeu[8]
 }
 function flipCard10() {
-    document.getElementById("card10").src = "../Images/cardFrontBlank.png"
+    document.getElementById("card10").src = cardValueJeu[9]
 }
 function flipCard11() {
-    document.getElementById("card11").src = "../Images/cardFrontBlank.png"
+    document.getElementById("card11").src = cardValueJeu[10]
 }
 function flipCard12() {
-    document.getElementById("card12").src = "../Images/cardFrontBlank.png"
+    document.getElementById("card12").src = cardValueJeu[11]
 }
 function flipCard13() {
-    document.getElementById("card13").src = "../Images/cardFrontBlank.png"
+    document.getElementById("card13").src = cardValueJeu[12]
 }
 function flipCard14() {
-    document.getElementById("card14").src = "../Images/cardFrontBlank.png"
+    document.getElementById("card14").src = cardValueJeu[13]
 }
 function flipCard15() {
-    document.getElementById("card15").src = "../Images/cardFrontBlank.png"
+    document.getElementById("card15").src = cardValueJeu[14]
 }
 function flipCard16() {
-    document.getElementById("card16").src = "../Images/cardFrontBlank.png"
+    document.getElementById("card16").src = cardValueJeu[15]
 }
 function flipCard17() {
-    document.getElementById("card17").src = "../Images/cardFrontBlank.png"
+    document.getElementById("card17").src = cardValueJeu[16]
 }
 function flipCard18() {
-    document.getElementById("card18").src = "../Images/cardFrontBlank.png"
+    document.getElementById("card18").src = cardValueJeu[17]
 }
 function flipCard19() {
-    document.getElementById("card19").src = "../Images/cardFrontBlank.png"
+    document.getElementById("card19").src = cardValueJeu[18]
 }
 function flipCard20() {
-    document.getElementById("card20").src = "../Images/cardFrontBlank.png"
+    document.getElementById("card20").src = cardValueJeu[19]
 }
 function flipCard21() {
-    document.getElementById("card21").src = "../Images/cardFrontBlank.png"
+    document.getElementById("card21").src = cardValueJeu[20]
 }
 function flipCard22() {
-    document.getElementById("card22").src = "../Images/cardFrontBlank.png"
+    document.getElementById("card22").src = cardValueJeu[21]
 }
 function flipCard23() {
-    document.getElementById("card23").src = "../Images/cardFrontBlank.png"
+    document.getElementById("card23").src = cardValueJeu[22]
 }
 function flipCard24() {
-    document.getElementById("card24").src = "../Images/cardFrontBlank.png"
+    document.getElementById("card24").src = cardValueJeu[23]
 }
 function flipCard25() {
-    document.getElementById("card25").src = "../Images/cardFrontBlank.png"
+    document.getElementById("card25").src = cardValueJeu[24]
 }
 function flipCard26() {
-    document.getElementById("card26").src = "../Images/cardFrontBlank.png"
+    document.getElementById("card26").src = cardValueJeu[25]
 }
 function flipCard27() {
-    document.getElementById("card27").src = "../Images/cardFrontBlank.png"
+    document.getElementById("card27").src = cardValueJeu[26]
 }
 function flipCard28() {
-    document.getElementById("card28").src = "../Images/cardFrontBlank.png"
+    document.getElementById("card28").src = cardValueJeu[27]
 }
 function flipCard29() {
-    document.getElementById("card29").src = "../Images/cardFrontBlank.png"
+    document.getElementById("card29").src = cardValueJeu[28]
 }
 function flipCard30() {
-    document.getElementById("card30").src = "../Images/cardFrontBlank.png"
+    document.getElementById("card30").src = cardValueJeu[29]
 }
 function flipCard31() {
-    document.getElementById("card31").src = "../Images/cardFrontBlank.png"
+    document.getElementById("card31").src = cardValueJeu[30]
 }
 function flipCard32() {
-    document.getElementById("card32").src = "../Images/cardFrontBlank.png"
+    document.getElementById("card32").src = cardValueJeu[31]
 }
 function flipCard33() {
-    document.getElementById("card33").src = "../Images/cardFrontBlank.png"
+    document.getElementById("card33").src = cardValueJeu[32]
 }
 function flipCard34() {
-    document.getElementById("card34").src = "../Images/cardFrontBlank.png"
+    document.getElementById("card34").src = cardValueJeu[33]
 }
 function flipCard35() {
-    document.getElementById("card35").src = "../Images/cardFrontBlank.png"
+    document.getElementById("card35").src = cardValueJeu[34]
 }
 function flipCard36() {
-    document.getElementById("card36").src = "../Images/cardFrontBlank.png"
+    document.getElementById("card36").src = cardValueJeu[35]
 }
 
 affiche = document.getElementById("divScore");
