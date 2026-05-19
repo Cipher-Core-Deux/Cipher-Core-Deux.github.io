59d9612cda92adf38eb55668b3e2d34ac04d4daa
Jack <heajac29@ecolecatholique.ca>
2026-05-19 14:02:31 -0400

    dsa

--- JS/script.js
+++ JS/script.js
@@ -57,7 +57,7 @@ function randomizer() {
         cardValue.splice(j, 1)
     }
     cardValue = ["../Images/Cards/Math/1.png", "../Images/Cards/Math/R1.png", "../Images/Cards/Math/2.png", "../Images/Cards/Math/R2.png", "../Images/Cards/Math/3.png", "../Images/Cards/Math/R3.png", "../Images/Cards/Math/4.png", "../Images/Cards/Math/R4.png", "../Images/Cards/Math/5.png", "../Images/Cards/Math/R5.png", "../Images/Cards/Math/6.png", "../Images/Cards/Math/R6.png", "../Images/Cards/Math/7.png", "../Images/Cards/Math/R7.png", "../Images/Cards/Math/8.png", "../Images/Cards/Math/R8.png", "../Images/Cards/Math/9.png", "../Images/Cards/Math/R9.png", "../Images/Cards/Math/10.png", "../Images/Cards/Math/R10.png", "../Images/Cards/Math/11.png", "../Images/Cards/Math/R11.png", "../Images/Cards/Math/12.png", "../Images/Cards/Math/R12.png", "../Images/Cards/Math/13.png", "../Images/Cards/Math/R13.png", "../Images/Cards/Math/14.png", "../Images/Cards/Math/R14.png", "../Images/Cards/Math/15.png", "../Images/Cards/Math/R15.png", "../Images/Cards/Math/16.png", "../Images/Cards/Math/R16.png", "../Images/Cards/Math/17.png", "../Images/Cards/Math/R17.png", "../Images/Cards/Math/18.png", "../Images/Cards/Math/R18.png"]
-    affiche.innerHTML = "0 / 18"
+    affiche.innerHTML = "Matches : 0 / 18"
     score = 0
 }
 let preCard = 0
@@ -94,7 +94,7 @@ function cardCheck(cardNum) {
                 console.log(score)
                 preCard = 0
                 flip = 0
-                scoreCard = score + "/ 18"
+                scoreCard = "Matches : " + score + "/ 18"
                 affiche.innerHTML = scoreCard
             } else {
                 preCard = 0
