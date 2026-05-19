a3047a30c94b9d26af2c7e712659215e04dd32f5
phabin29 <phabin29@ecolecatholique.ca>
2026-05-19 14:04:36 -0400

    Update music.js

--- JS/music.js
+++ JS/music.js
@@ -1,1 +1,12 @@
+/* https://discourse.webflow.com/t/add-background-music-to-page/169554/3 */
 
+var source = "audio/burger.mp3"
+ var audio = document.createElement("audio");
+ //
+ audio.autoplay = true;
+ //
+ audio.load()
+ audio.addEventListener("load", function() { 
+     audio.play(); 
+ }, true);
+ audio.src = source;
