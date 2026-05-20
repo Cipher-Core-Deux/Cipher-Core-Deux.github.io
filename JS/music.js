1f472259a6c92cb494b15493fb342c46c3f53580
phabin29 <phabin29@ecolecatholique.ca>
2026-05-20 11:40:17 -0400

    Update music.js

--- JS/music.js
+++ JS/music.js
@@ -2,14 +2,12 @@ var music = new Audio("Audio/backgroundMusic.mp3");
 
 music.loop = true;
 
-// Get saved song time
 var savedTime = sessionStorage.getItem("musicTime");
 
 if (savedTime) {
     music.currentTime = savedTime;
 }
 
-// Play music
 music.play();
 
 // Save song time every second
